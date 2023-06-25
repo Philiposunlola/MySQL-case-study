5bec9d00085e4258b7245520fe9ea5611e2d9a6c
Philip <philiposun@gmail.com>
2023-06-26 00:41:01 +0100

    MySQL-case-study

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }else {
+        echo "Connected successfully";
+    }
+    $mysqli_close($conn);
+?>
