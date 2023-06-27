221349a99be2d7bbdaac25ec14a4b617d33e6758
Philip <philiposun@gmail.com>
2023-06-28 00:46:23 +0100

    Update selectData.php

--- selectData.php
+++ selectData.php
@@ -1,6 +1,6 @@
 <?php
     $servername = "localhost";
-    $username = "username";
+    $username = "Philip Osunlola";
     $password = "YES";
     $dbname = "dbname";
 
@@ -17,7 +17,7 @@
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while ($row = mysqli_fetch_assoc($result) ) {
-            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+            echo "id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
         } 
     } else {
         echo "0 results";
