432e8fd6d2360e6386039591c42b701e932b1509
Philip <philiposun@gmail.com>
2023-06-27 01:39:34 +0100

    Create selectData.php

---
+++ selectData.php
@@ -0,0 +1,27 @@
+<?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "dbname";
+
+    // Create   connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id, firstname, lastname FROM Visitors";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result) ) {
+            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        } 
+    } else {
+        echo "0 results";
+    }
+
+    mysqli_close($conn);
+?>
