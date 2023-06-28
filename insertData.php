5826326edf7a69395a07273f18839c8d85dfc5cf
Philip <philiposun@gmail.com>
2023-06-28 02:31:36 +0100

    SQL

--- insertData.php
+++ insertData.php
@@ -1,8 +1,8 @@
 <?php
     $servername = "localhost";
     $username = "username";
-    $password = "password";
-    $dbname = "dbname";
+    $password = "YES";
+    $dbname = "studyBD";
 
     // Create   connection
     $conn = new mysqli($servername, $username, $password, $dbname);
--- where.php
+++ where.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "Philip Osunlola";
     $password = "YES";
-    $dbname = "Visitors";
+    $dbname = "studydb";
 
     // Create   connection
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,13 +11,13 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT id, firstname, lastname FROM Visitors WHERE lastname='Osunlola'";
+    $sql = "SELECT id, firstName, lastName FROM Visitors WHERE lastName='Osunlola'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while ($row = mysqli_fetch_assoc($result) ) {
-            echo "id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+            echo "id: ". $row["id"]. " - Name: ". $row["firstName"]. " " . $row["lastName"] . "<br>";
         } 
     } else {
         echo "0 results";
