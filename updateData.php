ba6f53e05132e80e53fd9cc8b43b20c3b709f380
Philip <philiposun@gmail.com>
2023-07-03 01:59:37 +0100

    Update updateData.php

--- updateData.php
+++ updateData.php
@@ -4,5 +4,20 @@
     $password = "";
     $dbname = "studydb";
 
-    
+   // Create connection 
+   $conn = $mysqli_connect($servername, $username, $password, $dbname);
+    // check connection 
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "UPDATE visitors SET lastName='Osunlola' WHERE id=2;
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Record update successfully";
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 ?>
